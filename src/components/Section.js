666d5c4d27e16e8ec9910c54b4d505b11170f5ae
Marinessa393 <futurism1303@gmail.com>
2021-03-08 20:31:38 +0200

    add propTypes

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../App.css';
 
 function Section(props) {
@@ -10,5 +11,10 @@ function Section(props) {
     </>
   );
 }
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+};
 
 export default Section;
